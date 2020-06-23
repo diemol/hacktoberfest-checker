a22826e8a5653d263d9191f7d0323a12dcf2d0bc
Diego Molina <diemol@gmail.com>
2020-06-24 00:58:35 +0200

    First color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-blue': 'rgb(21, 35, 71)',
+        'light-grey': '#EDEDEF',
         'dark-blue-alt': '#1d2c4e',
         'light-blue': '#92EAFF',
-        'mid-blue': '#39486E',
+        'mid-blue': '#003A70',
         'mid-purple': '#A11EC6',
         'mid-grey': '#99a1b3',
         'yellow': '#fff922'
