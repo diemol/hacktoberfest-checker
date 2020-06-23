b47d7f50dc9642d4f0f0f1446a7a471611e99790
Diego Molina <diemol@gmail.com>
2020-06-24 00:59:33 +0200

    Changing site title

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -3,11 +3,11 @@ import React from 'react';
 const SiteTitle = () => (
   <div className="text-center">
     <a className="no-underline inline-block py-12 md:py-8" href="/">
-      <h1 className="text-blue-700 text-4xl md:text-6xl">
-        Hacktober<span className="text-mid-purple">fest</span>
+      <h1 className="text-dark-grey text-4xl md:text-6xl">
+        OpenSource<span className="text-sauce-red">thon</span>
       </h1>
-      <small className="block text-right -mt-4 mr-8 text-yellow text-base italic">
-        Checker
+      <small className="block text-right -mt-4 mr-8 text-sauce-blue text-base italic">
+        #HFH2020 Edition
       </small>
     </a>
   </div>
