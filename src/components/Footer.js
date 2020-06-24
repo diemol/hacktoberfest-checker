e503f710bb0c385a3d234f55e92764c7a96bea48
Diego Molina <diemol@gmail.com>
2020-06-24 09:51:45 +0200

    Adjusting footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,32 @@ import React from 'react';
 const Footer = () => (
   <footer className="text-sm px-8 text-center flex-none py-4">
     <p className="text-dark-grey">
-      Disclaimer: This site is fan made and not affiliated with{' '}
+      Disclaimer:
+    </p>
+    <p className="text-dark-grey">
+      This site is a fork of {' '}
+      <a
+        href="https://github.com/jenkoian/hacktoberfest-checker"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        hacktoberfest-checker
+      </a>
+      .
+    </p>
+    <p className="text-dark-grey">
+      Made for the Sauce Labs 2020 Hackathon. See the code {' '}
+      <a
+        href="https://github.com/diemol/hacktoberfest-checker"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        here
+      </a>
+      .
+    </p>
+    <p className="text-dark-grey">
+      Not affiliated with{' '}
       <a
         href="https://hacktoberfest.digitalocean.com/"
         target="_blank"
