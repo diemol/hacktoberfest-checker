f12160d3e0739dd0179f2145e57ebb85a20016d8
Diego Molina <diemol@gmail.com>
2020-06-24 10:24:23 +0200

    Fixing layout

--- src/pages/User/components/PullRequests/ErrorText.js
+++ src/pages/User/components/PullRequests/ErrorText.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ErrorText = ({ errorMessage }) => (
-  <h2 className="text-sauce-red text-white">{errorMessage}</h2>
+  <h2 className="text-center text-sauce-red">{errorMessage}</h2>
 );
 
 ErrorText.propTypes = {
