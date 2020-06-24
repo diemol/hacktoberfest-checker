b5b66f6e9e27df1944cbc5f27c2544c4b9d4c17c
Diego Molina <diemol@gmail.com>
2020-06-24 17:53:50 +0200

    Adding extra link

--- src/pages/User/components/PullRequests/IssuesLink.js
+++ src/pages/User/components/PullRequests/IssuesLink.js
@@ -3,13 +3,20 @@ import React from 'react';
 const IssuesLink = () => (
   <div className="flex flex-wrap justify-center content-center text-center">
     <div className="text-dark-grey pb-4">
-      Look at the following{' '}
+      Look at the following issues{' '}
       <a
-        href="https://github.com/search?q=label:hacktoberfest+state:open+type:issue"
+        href="https://github.com/search?q=label:up-for-grabs+state:open+type:issue"
         target="_blank"
         rel="noopener noreferrer"
       >
-        issues
+        here
+      </a>{' & '}
+      <a
+        href="https://up-for-grabs.net"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        here
       </a>{' '}
       and get hacking!
     </div>
