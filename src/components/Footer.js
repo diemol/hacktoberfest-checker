1bd6383562441a10424ba9daef320e3d0adc6ab2
christian-bromann <mail@christian-bromann.com>
2020-06-25 14:06:37 +0200

    add some help notes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,41 @@ import sauceLogo from './saucelabs_logo_color.png';
 
 const Footer = () => (
   <footer className="text-sm px-8 text-center flex-none py-4">
-    <p className="text-dark-grey">
-      Disclaimer:
+    <b className="text-dark-grey">You need tasks to work on?</b>
+    <p>
+      We have collected a list of tasks for projects that our customers use
+      on daily basis.
     </p>
+    <p>
+      Working on these can make a direct impact on the customer experience:
+    </p>
+    <p style={{padding: "15px 0"}}>
+      <p>
+      <a href="https://github.com/seleniumhq/selenium/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc">Selenium Issue List</a>
+      </p>
+      <p>
+      <a href="https://github.com/webdriverio/webdriverio/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc+label%3A%22good+first+pick%22">"Good First Pick" WebdriverIO</a>
+      </p>
+    </p>
+    <b className="text-dark-grey">Need Support?</b>
+    <p>
+      Got stuck somewhere? No idea what to do? No worries, we are here to help!
+    </p>
+    <p>
+      Join the <i>#opensourcethon</i> Slack channel and let us know, the OpenSource team is
+      happy to pair with you!
+    </p>
+    <p>
+      Here are some other useful resources:
+    </p>
+    <p style={{padding: "15px 0"}}>
+      <p>
+      <a href="https://drive.google.com/file/d/1ee2nVN_5mp32L-ENErRJyHVkDcpvtDST/view?usp=sharing">"Contribute to WebdriverIO" Workshop Recordings</a>
+      </p>
+    </p>
+    <b className="text-dark-grey">
+      Disclaimer:
+    </b>
     <p className="text-dark-grey">
       This site is a fork of {' '}
       <a
